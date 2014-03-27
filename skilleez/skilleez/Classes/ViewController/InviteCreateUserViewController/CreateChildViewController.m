b9158db980103c7930e291d42a68c9e524c6285f
darkvas <Admin@Tolja-Mac.local>
2014-03-27 12:35:16 +0200

    CreateChildView add work with keyboard - move textfields up and down

--- skilleez/skilleez/Classes/ViewController/InviteCreateUserViewController/CreateChildViewController.m
+++ skilleez/skilleez/Classes/ViewController/InviteCreateUserViewController/CreateChildViewController.m
@@ -9,7 +9,12 @@
 #import "CreateChildViewController.h"
 #import "AppDelegate.h"
 
+#define kOFFSET_FOR_KEYBOARD 200.0
+
 @interface CreateChildViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *tfAccountId;
+@property (weak, nonatomic) IBOutlet UITextField *tfAccoundPass;
 
 @end
 
@@ -29,7 +34,20 @@
     [super viewDidLoad];
     
     [[AppDelegate alloc] cutomizeNavigationBar:self withTitle:@"New Child" leftTitle:@"Cancel" rightButton:YES rightTitle:@""];
+    
 	// Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,5 +61,49 @@
     self.navigationController.navigationBarHidden = NO;
     [self.navigationController popViewControllerAnimated:YES];
 }
+
+#pragma mark Keyboard show/hide
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
+}
+
+-(void)keyboardWillShow
+{
+    if (self.view.frame.origin.y >= 0) {
+        [self setViewMovedUp:YES];
+    } else if (self.view.frame.origin.y < 0) {
+        [self setViewMovedUp:NO];
+    }
+}
+
+-(void)keyboardWillHide
+{
+    if (self.view.frame.origin.y >= 0) {
+        [self setViewMovedUp:YES];
+    } else if (self.view.frame.origin.y < 0) {
+        [self setViewMovedUp:NO];
+    }
+}
+
+-(void)setViewMovedUp:(BOOL)movedUp
+{
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.3];
+    
+    CGRect rect = self.view.frame;
+    if (movedUp) {
+        rect.origin.y -= kOFFSET_FOR_KEYBOARD;
+        rect.size.height += kOFFSET_FOR_KEYBOARD;
+    } else {
+        rect.origin.y += kOFFSET_FOR_KEYBOARD;
+        rect.size.height -= kOFFSET_FOR_KEYBOARD;
+    }
+    self.view.frame = rect;
+    
+    [UIView commitAnimations];
+}
+
 
 @end
