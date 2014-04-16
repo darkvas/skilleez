b70efdab88b19b4b148d61c3a2ea0346af6869d4
darkvas <Admin@Tolja-Mac.local>
2014-04-16 14:32:07 +0300

    changed plist for docs type

--- skilleez/skilleez/AppDelegate.m
+++ skilleez/skilleez/AppDelegate.m
@@ -67,4 +67,10 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    NSLog(@"URL: %@ - app: %@", url, sourceApplication);
+    return YES;
+}
+
 @end
